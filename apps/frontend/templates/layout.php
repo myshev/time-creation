66a592d06f56dd1458ad7ab5a2de3ba9cd128009
Myshev <sergey.myshev@softline.ru>
2013-06-11 18:15:45 +0400

    user

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -9,6 +9,20 @@
     <?php include_javascripts() ?>
   </head>
   <body>
+  	<div id="header">
+		<? $class = sfConfig::get('app_sf_guard_plugin_signin_form', 'sfGuardFormSignin'); // получаем объект формы
+		$form = new $class();?>
+		<? if(!$sf_user->isAuthenticated()) {?>
+			<?=get_partial('sfGuardAuth/signin_form', array('form' => $form)) ?>
+
+			<a href="<?=url_for('sf_guard_register');?>" title="<?=__('Зарегаться');?>">Зарегаться</a>
+		<? } else { ?>
+			<span><?=__('Здравствуйте');?>, <?=$sf_user->getName();?> <?=$sf_user->getAttribute( 'email_address', null, 'sfGuardSecurityUser');?></span>
+			&nbsp;
+			<?=link_to(__('Выход'), 'sf_guard_signout'); ?>
+		<? } ?>
+  	</div>
+	<div class="clear"></div>
   	<div>
 		<?include_component('default', 'mainMenu');?>
   	</div>
